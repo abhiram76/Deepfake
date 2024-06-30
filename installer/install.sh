095742e2f82d6faaaa137bc6608e0715f137bb78
Abhiram <vortex.udc@gmail.com>
2024-06-30 20:16:53 +0530

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -2,6 +2,8 @@
 
 #installing cuda toolkit 11-8
 #!/bin/sh
+printf "installer script by abhiram76\n"
+printf "installing packages"
 apt-get -y install cuda-toolkit-11-8
 import os
 os.environ["LD_LIBRARY_PATH"] += ":" + "/usr/local/cuda-11/lib64"
@@ -9,17 +11,17 @@ os.environ["LD_LIBRARY_PATH"] += ":" + "/usr/local/cuda-11.8/lib64"
 
 # install faceswap and required packages 
 git clone https://github.com/vortex-udc/Roop-webui.git
-!apt install cmatrix
+apt install cmatrix
 
 
 # change directory 
-%cd Roop-webui
-!mv config_colab.yaml config.yaml
+cd Roop-webui
+mv config_colab.yaml config.yaml
 
 
 
 # install requirements to run (roop-unleashed)
-!pip install pip install -r requirements.txt
-
+pip install pip install -r requirements.txt
+printf "Installation Finished\n"
 
 #End script
