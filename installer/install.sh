086df940fbaccc5c1b1b22cd6242a68b02743f29
Abhiram <vortex.udc@gmail.com>
2024-06-30 20:28:32 +0530

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -3,13 +3,13 @@
 #installing cuda toolkit 11-8
 #!/bin/sh
 
-printf "installer script by abhiram76\n"
-printf "installing packages"
+echo "installer script by abhiram76\n"
+echo "installing packages"
 apt-get -y install cuda-toolkit-11-8
 import os
 os.environ["LD_LIBRARY_PATH"] += ":" + "/usr/local/cuda-11/lib64"
 os.environ["LD_LIBRARY_PATH"] += ":" + "/usr/local/cuda-11.8/lib64"
-
+clear
 
 mv config_colab.yaml config.yaml
 
@@ -17,6 +17,8 @@ mv config_colab.yaml config.yaml
 
 # install requirements to run (roop-unleashed)
 pip install pip install -r requirements.txt
-printf "Installation Finished\n"
+clear
+echo "Installation Finished\n"
+echo "Start the web interface now!\n"
 
 #End script
