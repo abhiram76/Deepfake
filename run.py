d33229cf3effed957968aea8f409e3dba4d3861e
Abhiram <vortex.udc@gmail.com>
2024-07-01 22:08:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from roop import core
-printf("Not started")
-printf("Server issue try again later")
-printf("      ")
-printf("      ")
-printf("try again later")
+print("Not started")
+print("Server issue try again later")
+print("      ")
+print("      ")
+print("try again later")
