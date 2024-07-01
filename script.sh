3ecef654514f46fa47f29d4fb2aadc76a09166f4
Abhiram <vortex.udc@gmail.com>
2024-07-01 12:05:00 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 #install script by abhiram76
 
 !apt-get -y install cuda-toolkit-11-8
-!python /content/Faceswap-AI/roop/cuda.py
-!bash /content/Faceswap-AI/roop/requirements.sh
+!mv config_colab.yaml config.yaml
+!pip install pip install -r requirements.txt
 #end script
