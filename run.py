ce12446e3d084d7235ed1fd1c48cfbf19e29d2bc
Abhiram <vortex.udc@gmail.com>
2024-07-02 14:35:58 +0530

    run

--- run.py
+++ run.py
@@ -2,7 +2,12 @@
 
 from roop import core
 print("starting")
-print("        ")
-print("      ")
-print("      ")
-print("       ")
+# start roop/core.py
+
+
+
+# from roop import core
+# start.sh   
+# print(starting)   web
+
+print("collab disconnected")
