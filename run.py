be8578f90e289cddc0b56a627b1fa3322b010529
Abhiram <vortex.udc@gmail.com>
2024-07-02 21:36:52 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
 from roop import core
-print("starting")
-# start roop/core.py
 
-
-
-# from roop import core
-# start.sh   
-# print(starting)   web
-
-print("collab disconnected")
+if __name__ == '__main__':
+    core.run()
+  
+#starting web interface 
+print("Connected to GPU Runtime")
