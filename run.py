bdd6d69af045ed418d23b6ac1219f1617418a3cc
Abhiram <vortex.udc@gmail.com>
2024-07-03 23:06:15 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 from roop import core
-# start core.py
-    # ()
 
-  
-#starting web interface 
-print("Connected to GPU Runtime")
-print("Start failed")
+if __name__ == '__main__':
+    core.run()
+print("last updated : 3 July 2024 (23:05)")
+print("connected to gpu runtime")
+print("starting core.py")
